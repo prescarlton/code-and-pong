aed3aa3d9adcc24e705531e8c20066f37207aa73
Preston Carlton <prescarlton@gmail.com>
2024-09-19 22:05:48 -0400

    upd middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 
-const isProtectedRoute = createRouteMatcher(["/score-keeper(.*)"])
+const isProtectedRoute = createRouteMatcher([
+  "/score-keeper(.*)",
+  "/past-round(.*)",
+])
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect()
