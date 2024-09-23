47a29e7b978321fc5f0c39229380bce8e8435765
Preston Carlton <prescarlton@gmail.com>
2024-09-23 13:11:19 -0400

    adds sign in cta

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -1,13 +1,28 @@
 import { UserButton } from "@clerk/nextjs"
+import { auth } from "@clerk/nextjs/server"
 import Link from "next/link"
+import { Button } from "./ui/button"
 
 const Topbar = () => {
+  const { userId } = auth()
   return (
     <div className="flex items-center justify-between px-8 py-4 fixed w-full">
       <Link href="/">
         <h2 className="text-2xl font-bold">🏓 CODE/+/PONG</h2>
       </Link>
-      <UserButton />
+      {userId ? (
+        <UserButton />
+      ) : (
+        <div className="flex items-center gap-2">
+          <Link href="/sign-in">
+            <Button variant="ghost">Sign In</Button>
+          </Link>
+
+          <Link href="/sign-up">
+            <Button>Sign Up</Button>
+          </Link>
+        </div>
+      )}
     </div>
   )
 }
